06004c79b4b0e791e03d18ede62b28dd96e03f7e
Dane Lacey <daneclacey@gmail.com>
2024-06-19 17:14:45 +0200

    add some handy scripts

---
+++ code/scripts/scrape_bandwidth.py
@@ -0,0 +1,27 @@
+import pprint
+
+bandwidths = {}
+data_vols = {}
+
+with open('uspmv_cpu_likwid_bandwidths.txt') as file:
+    Lines=file.readlines()
+    line_count = 0
+    for line in Lines:
+        if "Region Info" in line:
+            thread_count = line.split()[-2]
+            if thread_count not in bandwidths.keys():
+                bandwidths[thread_count] = []
+            if thread_count not in data_vols.keys():
+                data_vols[thread_count] = []
+            try:
+                bandwidths[thread_count].append(float(Lines[line_count + 82].split()[6]))
+                data_vols[thread_count].append(float(Lines[line_count + 83].split()[7]))
+            except:
+                bandwidths[thread_count].append("Fill manually")
+                data_vols[thread_count].append("Fill manually") 
+        line_count += 1
+
+print("bandwidths")
+pprint.pprint(bandwidths)
+print("Data volume")
+pprint.pprint(data_vols)
