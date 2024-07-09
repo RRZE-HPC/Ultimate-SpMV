0585bea55808badbf0c0a0a11069ce73a540f18a
Dane Lacey <daneclacey@gmail.com>
2024-07-09 15:41:45 +0200

    add GPU kernels in interface

--- code/interface.hpp
+++ code/interface.hpp
@@ -543,117 +543,6 @@ void convert_to_scs(
     // return true;
 }
 
-// SpMV kernels for computing  y = A * x, where A is a sparse matrix
-// represented by different formats.
-
-/**
- * Kernel for CSR format.
- */
-template <typename VT, typename IT>
-static void
-spmv_omp_csr(const ST C, // 1
-             const ST num_rows, // n_chunks
-             const IT * RESTRICT row_ptrs, // chunk_ptrs
-             const IT * RESTRICT row_lengths, // unused
-             const IT * RESTRICT col_idxs,
-             const VT * RESTRICT values,
-             VT * RESTRICT x,
-             VT * RESTRICT y)
-{
-    // Orphaned directive: Assumed already called within a parallel region
-    #pragma omp for schedule(static)
-    for (ST row = 0; row < num_rows; ++row) {
-        VT sum{};
-
-        #pragma omp simd simdlen(VECTOR_LENGTH) reduction(+:sum)
-        for (IT j = row_ptrs[row]; j < row_ptrs[row + 1]; ++j) {
-            sum += values[j] * x[col_idxs[j]];
-        }
-        y[row] = sum;
-    }
-}
-
-/**
- * SpMV Kernel for Sell-C-Sigma. Supports all Cs > 0.
- */
-template <typename VT, typename IT>
-static void
-spmv_omp_scs(const ST C,
-             const ST n_chunks,
-             const IT * RESTRICT chunk_ptrs,
-             const IT * RESTRICT chunk_lengths,
-             const IT * RESTRICT col_idxs,
-             const VT * RESTRICT values,
-             VT * RESTRICT x,
-             VT * RESTRICT y)
-{
-
-    // Orphaned directive: Assumed already called within a parallel region
-    #pragma omp for schedule(static)
-    for (ST c = 0; c < n_chunks; ++c) {
-        VT tmp[C];
-        for (ST i = 0; i < C; ++i) {
-            tmp[i] = VT{};
-        }
-
-        IT cs = chunk_ptrs[c];
-
-        for (IT j = 0; j < chunk_lengths[c]; ++j) {
-            for (IT i = 0; i < (IT)C; ++i) {
-                tmp[i] += values[cs + j * (IT)C + i] * x[col_idxs[cs + j * (IT)C + i]];
-            }
-        }
-
-        for (ST i = 0; i < C; ++i) {
-            y[c * C + i] = tmp[i];
-        }
-    }
-}
-
-template <typename IT>
-static void
-spmv_omp_csr_mp(
-    const ST hp_n_rows, // TODO: (same for both)
-    const ST hp_C, // 1
-    const IT * RESTRICT hp_row_ptrs, // hp_chunk_ptrs
-    const IT * RESTRICT hp_row_lengths, // unused for now
-    const IT * RESTRICT hp_col_idxs,
-    const double * RESTRICT hp_values,
-    double * RESTRICT hp_x,
-    double * RESTRICT hp_y, 
-    const ST lp_n_rows, // TODO: (same for both)
-    const ST lp_C, // 1
-    const IT * RESTRICT lp_row_ptrs, // lp_chunk_ptrs
-    const IT * RESTRICT lp_row_lengths, // unused for now
-    const IT * RESTRICT lp_col_idxs,
-    const float * RESTRICT lp_values,
-    float * RESTRICT lp_x,
-    float * RESTRICT lp_y, // unused
-    int my_rank
-    )
-{
-    #pragma omp parallel for schedule(static)
-    for (ST row = 0; row < hp_n_rows; ++row) {
-        double hp_sum{};
-        #pragma omp simd simdlen(VECTOR_LENGTH) reduction(+:hp_sum)
-        for (IT j = hp_row_ptrs[row]; j < hp_row_ptrs[row+1]; ++j) {
-            hp_sum += hp_values[j] * hp_x[hp_col_idxs[j]];
-        }
-        
-
-        double lp_sum{};
-        // #pragma omp simd simdlen(2*VECTOR_LENGTH) reduction(+:lp_sum)
-        #pragma omp simd simdlen(VECTOR_LENGTH) reduction(+:lp_sum)
-        for (IT j = lp_row_ptrs[row]; j < lp_row_ptrs[row + 1]; ++j) {
-            // lp_sum += lp_values[j] * lp_x[lp_col_idxs[j]];
-            lp_sum += lp_values[j] * hp_x[lp_col_idxs[j]];
-
-        }
-
-        hp_y[row] = hp_sum + lp_sum; // implicit conversion to double
-    }
-}
-
 template <typename VT, typename IT>
 void apply_permutation(
     VT *permuted_vec,
@@ -794,4 +683,353 @@ void seperate_lp_from_hp(
         exit(1);
     }
 }
+
+////////////////////////////// CPU Kernels ////////////////////////////// 
+
+// SpMV kernels for computing  y = A * x, where A is a sparse matrix
+// represented by different formats.
+
+/**
+ * Kernel for CSR format.
+ */
+template <typename VT, typename IT>
+static void
+uspmv_omp_csr_cpu(const ST C, // 1
+             const ST num_rows, // n_chunks
+             const IT * RESTRICT row_ptrs, // chunk_ptrs
+             const IT * RESTRICT row_lengths, // unused
+             const IT * RESTRICT col_idxs,
+             const VT * RESTRICT values,
+             VT * RESTRICT x,
+             VT * RESTRICT y)
+{
+    // Orphaned directive: Assumed already called within a parallel region
+    #pragma omp for schedule(static)
+    for (ST row = 0; row < num_rows; ++row) {
+        VT sum{};
+
+        #pragma omp simd simdlen(VECTOR_LENGTH) reduction(+:sum)
+        for (IT j = row_ptrs[row]; j < row_ptrs[row + 1]; ++j) {
+            sum += values[j] * x[col_idxs[j]];
+        }
+        y[row] = sum;
+    }
+}
+
+/**
+ * SpMV Kernel for Sell-C-Sigma. Supports all Cs > 0.
+ */
+template <typename VT, typename IT>
+static void
+uspmv_omp_scs_cpu(const ST C,
+             const ST n_chunks,
+             const IT * RESTRICT chunk_ptrs,
+             const IT * RESTRICT chunk_lengths,
+             const IT * RESTRICT col_idxs,
+             const VT * RESTRICT values,
+             VT * RESTRICT x,
+             VT * RESTRICT y)
+{
+
+    // Orphaned directive: Assumed already called within a parallel region
+    #pragma omp for schedule(static)
+    for (ST c = 0; c < n_chunks; ++c) {
+        VT tmp[C];
+        for (ST i = 0; i < C; ++i) {
+            tmp[i] = VT{};
+        }
+
+        IT cs = chunk_ptrs[c];
+
+        for (IT j = 0; j < chunk_lengths[c]; ++j) {
+            for (IT i = 0; i < (IT)C; ++i) {
+                tmp[i] += values[cs + j * (IT)C + i] * x[col_idxs[cs + j * (IT)C + i]];
+            }
+        }
+
+        for (ST i = 0; i < C; ++i) {
+            y[c * C + i] = tmp[i];
+        }
+    }
+}
+
+
+/**
+ * Sell-C-sigma implementation templated by C.
+ */
+template <ST C, typename VT, typename IT>
+static void
+scs_impl_cpu(const ST n_chunks,
+         const IT * RESTRICT chunk_ptrs,
+         const IT * RESTRICT chunk_lengths,
+         const IT * RESTRICT col_idxs,
+         const VT * RESTRICT values,
+         const VT * RESTRICT x,
+         VT * RESTRICT y)
+{
+
+    #pragma omp parallel for schedule(static)
+    for (ST c = 0; c < n_chunks; ++c) {
+        VT tmp[C]{};
+
+        IT cs = chunk_ptrs[c];
+
+        for (IT j = 0; j < chunk_lengths[c]; ++j) {
+            #pragma omp simd
+            for (IT i = 0; i < C; ++i) {
+                tmp[i] += values[cs + j * C + i] * x[col_idxs[cs + j * C + i]];
+            }
+        }
+
+        #pragma omp simd
+        for (IT i = 0; i < C; ++i) {
+            y[c * C + i] = tmp[i];
+        }
+    }
+}
+
+
+/**
+ * Dispatch to Sell-C-sigma kernels templated by C.
+ *
+ * Note: only works for selected Cs, see INSTANTIATE_CS.
+ */
+template <typename VT, typename IT>
+static void
+uspmv_omp_scs_c_cpu(
+             const ST C,
+             const ST n_chunks,
+             const IT * RESTRICT chunk_ptrs,
+             const IT * RESTRICT chunk_lengths,
+             const IT * RESTRICT col_idxs,
+             const VT * RESTRICT values,
+             const VT * RESTRICT x,
+             VT * RESTRICT y)
+{
+    switch (C)
+    {
+        #define INSTANTIATE_CS X(2) X(4) X(8) X(16) X(32) X(64)
+
+        #define X(CC) case CC: scs_impl_cpu<CC>(n_chunks, chunk_ptrs, chunk_lengths, col_idxs, values, x, y); break;
+        INSTANTIATE_CS
+        #undef X
+
+#ifdef SCS_C
+    case SCS_C:
+        case SCS_C: scs_impl_cpu<SCS_C>(n_chunks, chunk_ptrs, chunk_lengths, col_idxs, values, x, y);
+        break;
+#endif
+    default:
+        fprintf(stderr,
+                "ERROR: for C=%ld no instantiation of a sell-c-sigma kernel exists.\n",
+                long(C));
+        exit(1);
+    }
+}
+
+template <typename IT>
+static void
+uspmv_omp_csr_ap(
+    const ST hp_n_rows, // TODO: (same for both)
+    const ST hp_C, // 1
+    const IT * RESTRICT hp_row_ptrs, // hp_chunk_ptrs
+    const IT * RESTRICT hp_row_lengths, // unused for now
+    const IT * RESTRICT hp_col_idxs,
+    const double * RESTRICT hp_values,
+    double * RESTRICT hp_x,
+    double * RESTRICT hp_y, 
+    const ST lp_n_rows, // TODO: (same for both)
+    const ST lp_C, // 1
+    const IT * RESTRICT lp_row_ptrs, // lp_chunk_ptrs
+    const IT * RESTRICT lp_row_lengths, // unused for now
+    const IT * RESTRICT lp_col_idxs,
+    const float * RESTRICT lp_values,
+    float * RESTRICT lp_x,
+    float * RESTRICT lp_y, // unused
+    int my_rank
+    )
+{
+    #pragma omp parallel for schedule(static)
+    for (ST row = 0; row < hp_n_rows; ++row) {
+        double hp_sum{};
+        #pragma omp simd simdlen(VECTOR_LENGTH) reduction(+:hp_sum)
+        for (IT j = hp_row_ptrs[row]; j < hp_row_ptrs[row+1]; ++j) {
+            hp_sum += hp_values[j] * hp_x[hp_col_idxs[j]];
+        }
+        
+
+        double lp_sum{};
+        // #pragma omp simd simdlen(2*VECTOR_LENGTH) reduction(+:lp_sum)
+        #pragma omp simd simdlen(VECTOR_LENGTH) reduction(+:lp_sum)
+        for (IT j = lp_row_ptrs[row]; j < lp_row_ptrs[row + 1]; ++j) {
+            // lp_sum += lp_values[j] * lp_x[lp_col_idxs[j]];
+            lp_sum += lp_values[j] * hp_x[lp_col_idxs[j]];
+
+        }
+
+        hp_y[row] = hp_sum + lp_sum; // implicit conversion to double
+    }
+}
+
+#ifdef __CUDACC__
+////////////////////////////// GPU Kernels //////////////////////////////
+/**
+ * Kernel for CSR format.
+ */
+template <typename VT, typename IT>
+__global__ 
+static void
+uspmv_csr_gpu(const ST num_rows,
+         const IT * RESTRICT row_ptrs,
+         const IT * RESTRICT col_idxs,
+         const VT * RESTRICT values,
+         const VT * RESTRICT x,
+         VT * RESTRICT y)
+{
+    ST row = threadIdx.x + blockDim.x * blockIdx.x;
+
+    if (row < num_rows) {
+        VT sum{};
+        for (IT j = row_ptrs[row]; j < row_ptrs[row + 1]; ++j) {
+            sum += values[j] * x[col_idxs[j]];
+        }
+        y[row] = sum;
+    }
+}
+
+/**
+ * Kernel for ELL format, data structures use column major (CM) layout.
+ */
+template <typename VT, typename IT>
+__global__
+static void
+uspmv_ell_cm_gpu(const ST num_rows,
+         const ST nelems_per_row,
+         const IT * RESTRICT col_idxs,
+         const VT * RESTRICT values,
+         const VT * RESTRICT x,
+         VT * RESTRICT y)
+{
+    ST row = threadIdx.x + blockDim.x * blockIdx.x;
+
+    if (row < num_rows) {
+        VT sum{};
+        for (ST i = 0; i < nelems_per_row; ++i) {
+            VT val = values[row + i * num_rows];
+            IT col = col_idxs[row + i * num_rows];
+
+            sum += val * x[col];
+        }
+        y[row] = sum;
+    }
+}
+
+
+/**
+ * Kernel for Sell-C-Sigma. Supports all Cs > 0.
+ */
+template <typename VT, typename IT>
+__global__
+static void
+uspmv_scs_gpu(const ST C,
+         const ST n_chunks,
+         const IT * RESTRICT chunk_ptrs,
+         const IT * RESTRICT chunk_lengths,
+         const IT * RESTRICT col_idxs,
+         const VT * RESTRICT values,
+         const VT * RESTRICT x,
+         VT * RESTRICT y)
+{
+    ST row = threadIdx.x + blockDim.x * blockIdx.x;
+    IT c   = row / C;  // the no. of the chunk
+    IT idx = row % C;  // index inside the chunk
+
+    if (row < n_chunks * C) {
+        VT tmp{};
+        IT cs = chunk_ptrs[c];
+
+        for (IT j = 0; j < chunk_lengths[c]; ++j) {
+            tmp += values[cs + j * C + idx] * x[col_idxs[cs + j * C + idx]];
+        }
+
+        y[row] = tmp;
+    }
+
+}
+
+// Advanced SpMV kernels for computing  y = A * x, where A is a sparse matrix
+// represented by different formats.
+
+/**
+ * Sell-C-sigma implementation templated by C.
+ */
+template <ST C, typename VT, typename IT>
+__device__
+static void
+scs_impl_gpu(const ST n_chunks,
+         const IT * RESTRICT chunk_ptrs,
+         const IT * RESTRICT chunk_lengths,
+         const IT * RESTRICT col_idxs,
+         const VT * RESTRICT values,
+         const VT * RESTRICT x,
+         VT * RESTRICT y)
+{
+    ST row = threadIdx.x + blockDim.x * blockIdx.x;
+    ST c   = row / C;  // the no. of the chunk
+    ST idx = row % C;  // index inside the chunk
+
+    if (row < n_chunks * C) {
+        VT tmp{};
+        IT cs = chunk_ptrs[c];
+
+        for (ST j = 0; j < chunk_lengths[c]; ++j) {
+            tmp += values[cs + j * C + idx] * x[col_idxs[cs + j * C +idx]];
+        }
+
+        y[row] = tmp;
+    }
+
+}
+
+
+/**
+ * Dispatch to Sell-C-sigma kernels templated by C.
+ *
+ * Note: only works for selected Cs, see INSTANTIATE_CS.
+ */
+template <typename VT, typename IT>
+__global__
+static void
+uspmv_scs_c_gpu(
+             const ST C,
+             const ST n_chunks,
+             const IT * RESTRICT chunk_ptrs,
+             const IT * RESTRICT chunk_lengths,
+             const IT * RESTRICT col_idxs,
+             const VT * RESTRICT values,
+             const VT * RESTRICT x,
+             VT * RESTRICT y)
+{
+    switch (C)
+    {
+        #define INSTANTIATE_CS X(2) X(4) X(8) X(16) X(32) X(64) X(128)
+
+        #define X(CC) case CC: scs_impl_gpu<CC>(n_chunks, chunk_ptrs, chunk_lengths, col_idxs, values, x, y); break;
+        INSTANTIATE_CS
+        #undef X
+
+#ifdef SCS_C
+    case SCS_C:
+        case SCS_C: scs_impl_gpu<SCS_C>(n_chunks, chunk_ptrs, chunk_lengths, col_idxs, values, x, y);
+        break;
+#endif
+    default:
+        //fprintf(stderr,
+        //        "ERROR: for C=%ld no instantiation of a sell-c-sigma kernel exists.\n",
+        //        long(C));
+        // exit(1);
+    }
+}
+#endif
+
 #endif /*INTERFACE_H*/
