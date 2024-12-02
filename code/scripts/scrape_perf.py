6f8f17157ce19fa313a9c38a0197f12556adb58e
Dane Lacey <daneclacey@gmail.com>
2024-12-02 14:53:04 +0100

    remove lower depth scripts dir

--- code/scripts/scrape_perf.py
+++
@@ -1,8 +0,0 @@
-
-with open('spmv_bench_AP_32_1024.txt') as file:
-    Lines=file.readlines()
-    line_count = 0
-    for line in Lines:
-        if "Total Gflops:" in line:
-            print(Lines[line_count + 2].split()[0], end=",\n")
-        line_count += 1
