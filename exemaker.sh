f9320b51249612366eabc14d6b03ddf1078e79ee
Sriram Katta <sriram.katta@fau.de>
2025-03-23 22:25:14 +0100

    refactor exemaker.sh to streamline configuration management and executable creation process

--- exemaker.sh
+++ exemaker.sh
@@ -2,57 +2,105 @@
 
 module load intel
 module load intelmpi
+module load likwid
 module load mkl
+
+RED="\e[31m"
+GREEN="\e[32m"
+RESET="\e[0m"
 
 exedir="./EXE"
 
-if [ ! -d "$exedir" ]
-then
-    mkdir -p $exedir
-fi
+[ ! -d "$exedir" ] && mkdir -p $exedir
 
+get_config_value() {
+  grep -i "$1 =" config.mk | awk '{print $NF}'
+}
 
-OLD_COMPILER=$(grep -i "COMPILER =" config.mk | awk '{print $NF}')  
-OLD_SIMD_LENGTH=$(grep -i "SIMD_LENGTH =" config.mk | awk '{print $NF}')  
-OLD_BLOCK_VECTOR_LAYOUT=$(grep -i "BLOCK_VECTOR_LAYOUT =" config.mk | awk '{print $NF}')  
-OLD_DEBUG_MODE=$(grep -i "DEBUG_MODE =" config.mk | awk '{print $NF}')  
-OLD_DEBUG_MODE_FINE=$(grep -i "DEBUG_MODE_FINE =" config.mk | awk '{print $NF}')  
-OLD_OUTPUT_SPARSITY=$(grep -i "OUTPUT_SPARSITY =" config.mk | awk '{print $NF}')  
-OLD_CPP_VERSION=$(grep -i "CPP_VERSION =" config.mk | awk '{print $NF}')  
-OLD_GPGPU_ARCH=$(grep -i "GPGPU_ARCH =" config.mk | awk '{print $NF}')  
-OLD_THREADS_PER_BLOCK=$(grep -i "THREADS_PER_BLOCK =" config.mk | awk '{print $NF}')   # GPU only
-OLD_USE_MKL=$(grep -i "USE_MKL =" config.mk | awk '{print $NF}')  
-OLD_USE_MPI=$(grep -i "USE_MPI =" config.mk | awk '{print $NF}')  
-OLD_USE_METIS=$(grep -i "USE_METIS =" config.mk | awk '{print $NF}')  
-OLD_USE_LIKWID=$(grep -i "USE_LIKWID =" config.mk | awk '{print $NF}')  
-OLD_USE_CUSPARSE=$(grep -i "USE_CUSPARSE =" config.mk | awk '{print $NF}')  
+update_config_value() {
+  local key=$1
+  local value=$2
+  sed -i 's/^\(# \?\)\?'"$key"' = .*/'"$key"' = '"$value"'/' config.mk
+}
 
-sed -i 's/^\(# \?\)\?USE_MKL = .*/USE_MKL = '"1"'/' config.mk
+#collecting all old values
+OLD_COMPILER=$(get_config_value "COMPILER")
+OLD_SIMD_LENGTH=$(get_config_value "SIMD_LENGTH")
+OLD_BLOCK_VECTOR_LAYOUT=$(get_config_value "BLOCK_VECTOR_LAYOUT")
+OLD_MPI_MODE=$(get_config_value "MPI_MODE")
+OLD_DEBUG_MODE=$(get_config_value "DEBUG_MODE")
+OLD_DEBUG_MODE_FINE=$(get_config_value "DEBUG_MODE_FINE")
+OLD_OUTPUT_SPARSITY=$(get_config_value "OUTPUT_SPARSITY")
+OLD_CPP_VERSION=$(get_config_value "CPP_VERSION")
+OLD_GPGPU_ARCH=$(get_config_value "GPGPU_ARCH")
+OLD_THREADS_PER_BLOCK=$(get_config_value "THREADS_PER_BLOCK") # GPU only
+OLD_USE_OPENMP=$(get_config_value "USE_OPENMP")
+OLD_USE_MKL=$(get_config_value "USE_MKL")
+OLD_USE_MPI=$(get_config_value "USE_MPI")
+OLD_USE_METIS=$(get_config_value "USE_METIS")
+OLD_USE_LIKWID=$(get_config_value "USE_LIKWID")
+OLD_USE_CUSPARSE=$(get_config_value "USE_CUSPARSE")
+OLD_USE_SCAMAC=$(get_config_value "USE_SCAMAC")
 
-#MPI off mkl on
-sed -i 's/^\(# \?\)\?USE_MPI = .*/USE_MPI = '"0"'/' config.mk
-make
-mv uspmv ./EXE/uspmv_mkl1_mpi0
+#setting to default values of config.mk
+update_config_value "COMPILER" "icx"
+update_config_value "SIMD_LENGTH" "4"
+update_config_value "BLOCK_VECTOR_LAYOUT" "rowwise"
+update_config_value "MPI_MODE" "singlevec"
+update_config_value "DEBUG_MODE" "0"
+update_config_value "DEBUG_MODE_FINE" "0"
+update_config_value "OUTPUT_SPARSITY" "0"
+update_config_value "CPP_VERSION" "c++14"
+update_config_value "GPGPU_ARCH" "none"
+update_config_value "THREADS_PER_BLOCK" "128"
+update_config_value "USE_OPENMP" "0"
+update_config_value "USE_MKL" "0"
+update_config_value "USE_MPI" "0"
+update_config_value "USE_METIS" "0"
+update_config_value "USE_LIKWID" "0"
+update_config_value "USE_CUSPARSE" "0"
+update_config_value "USE_SCAMAC" "0"
 
-#MPI on mkl on
-sed -i 's/^\(# \?\)\?USE_MPI = .*/USE_MPI = '"1"'/' config.mk
-make
-mv uspmv ./EXE/uspmv_mkl1_mpi1
+declare -a mpi_modes=("singlevec" "multivec" "bulkvec")
+declare -a active_modes=(1)
+declare -a deactive_modes=(0)
+declare -a binarymodes=(0)
 
+for mpi_mode in "${mpi_modes[@]}"; do
+  update_config_value "MPI_MODE" "$mpi_mode"
+  for mkl_set in "${binarymodes[@]}"; do
+    update_config_value "USE_MKL" "$mkl_set"
+    for mpi_set in "${binarymodes[@]}"; do
+      update_config_value "USE_MPI" "$mpi_set"
+      exename=uspmv_${mpi_mode}_MKL${mkl_set}_MPI${mpi_set}
+      make clean >/dev/null 2>&1 && make -j$(nproc) >/dev/null 2>&1
+      if [ $? -eq 0 ]; then
+        mv uspmv $exedir/$exename
+        echo -e "$exename executable creation ${GREEN}sucess${RESET}"
+      else
+        echo -e "$exename executable creation ${RED}failed${RESET}"
+      fi
+    done
+  done
+done
 
+make clean >/dev/null 2>&1
 
-
-sed -i 's/^\(# \?\)\?COMPILER = .*/COMPILER = '"$OLD_COMPILER"'/' config.mk
-sed -i 's/^\(# \?\)\?SIMD_LENGTH = .*/SIMD_LENGTH = '"$OLD_SIMD_LENGTH"'/' config.mk
-sed -i 's/^\(# \?\)\?BLOCK_VECTOR_LAYOUT = .*/BLOCK_VECTOR_LAYOUT = '"$OLD_BLOCK_VECTOR_LAYOUT"'/' config.mk
-sed -i 's/^\(# \?\)\?DEBUG_MODE = .*/DEBUG_MODE = '"$OLD_DEBUG_MODE"'/' config.mk
-sed -i 's/^\(# \?\)\?DEBUG_MODE_FINE = .*/DEBUG_MODE_FINE = '"$OLD_DEBUG_MODE_FINE"'/' config.mk
-sed -i 's/^\(# \?\)\?OUTPUT_SPARSITY = .*/OUTPUT_SPARSITY = '"$OLD_OUTPUT_SPARSITY"'/' config.mk
-sed -i 's/^\(# \?\)\?CPP_VERSION = .*/CPP_VERSION = '"$OLD_CPP_VERSION"'/' config.mk
-sed -i 's/^\(# \?\)\?GPGPU_ARCH = .*/GPGPU_ARCH = '"$OLD_GPGPU_ARCH"'/' config.mk
-sed -i 's/^\(# \?\)\?THREADS_PER_BLOCK = .*/THREADS_PER_BLOCK = '"$OLD_THREADS_PER_BLOCK"'/' config.mk
-sed -i 's/^\(# \?\)\?USE_MKL = .*/USE_MKL = '"$OLD_USE_MKL"'/' config.mk
-sed -i 's/^\(# \?\)\?USE_MPI = .*/USE_MPI = '"$OLD_USE_MPI"'/' config.mk
-sed -i 's/^\(# \?\)\?USE_METIS = .*/USE_METIS = '"$OLD_USE_METIS"'/' config.mk
-sed -i 's/^\(# \?\)\?USE_LIKWID = .*/USE_LIKWID = '"$OLD_USE_LIKWID"'/' config.mk
-sed -i 's/^\(# \?\)\?USE_CUSPARSE = .*/USE_CUSPARSE = '"$OLD_USE_CUSPARSE"'/' config.mk
+#reseting to old value of config.mk
+update_config_value "COMPILER" "$OLD_COMPILER"
+update_config_value "SIMD_LENGTH" "$OLD_SIMD_LENGTH"
+update_config_value "BLOCK_VECTOR_LAYOUT" "$OLD_BLOCK_VECTOR_LAYOUT"
+update_config_value "MPI_MODE" "$OLD_MPI_MODE"
+update_config_value "DEBUG_MODE" "$OLD_DEBUG_MODE"
+update_config_value "DEBUG_MODE_FINE" "$OLD_DEBUG_MODE_FINE"
+update_config_value "OUTPUT_SPARSITY" "$OLD_OUTPUT_SPARSITY"
+update_config_value "CPP_VERSION" "$OLD_CPP_VERSION"
+update_config_value "GPGPU_ARCH" "$OLD_GPGPU_ARCH"
+update_config_value "THREADS_PER_BLOCK" "$OLD_THREADS_PER_BLOCK"
+update_config_value "USE_OPENMP" "$OLD_USE_OPENMP"
+update_config_value "USE_MKL" "$OLD_USE_MKL"
+update_config_value "USE_MPI" "$OLD_USE_MPI"
+update_config_value "USE_METIS" "$OLD_USE_METIS"
+update_config_value "USE_LIKWID" "$OLD_USE_LIKWID"
+update_config_value "USE_CUSPARSE" "$OLD_USE_CUSPARSE"
+update_config_value "USE_SCAMAC" "$OLD_USE_SCAMAC"
